63020ebd51e6880be89b4749e43ea01e201a554e
Tristan Chase <tristan.m.chase@gmail.com>
2020-04-08 21:39:34 -0400

    Change /tmp to "$HOME"/tmp

--- boilerplate-3.sh
+++ boilerplate-3.sh
@@ -7,7 +7,7 @@ expr "$*" : ".*--help" > /dev/null && usage
 #-----------------------------------
 # Low-tech logging function
 
-readonly LOG_FILE="/tmp/$(basename "$0").log"
+readonly LOG_FILE=""$HOME"/tmp/$(basename "$0").log"
 info()    { echo "[INFO]    $*" | tee -a "$LOG_FILE" >&2 ; }
 warning() { echo "[WARNING] $*" | tee -a "$LOG_FILE" >&2 ; }
 error()   { echo "[ERROR]   $*" | tee -a "$LOG_FILE" >&2 ; }
