5834ad800543366d223498e87bf794a5ad088dae
Tristan Chase <tristan.m.chase@gmail.com>
2020-08-03 00:24:11 -0400

    Create template file for refactoring new-script.sh

---
+++ template.sh
@@ -0,0 +1,29 @@
+shebang
+desc sec
+  name
+  usage()
+    usage
+    desc
+    examples
+    options
+  create date
+  created by
+  depends on
+  changelog
+todo
+lic sec
+  copyright
+  lic
+shared functions (source) 
+  debug
+  logging
+  trap functions
+  dep
+local functions
+sanity check
+  be root?
+  initialize variables
+process options
+bash settings
+main script wrapper
+main script
